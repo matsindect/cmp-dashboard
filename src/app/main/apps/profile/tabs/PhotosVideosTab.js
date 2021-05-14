54cfd577dc41071df6c9d89bcbdbce91c190a281
Theo Nga <tdbvume@outlook.com>
2021-05-14 14:32:02 +0200

    profile media files

--- src/app/main/apps/profile/tabs/PhotosVideosTab.js
+++ src/app/main/apps/profile/tabs/PhotosVideosTab.js
@@ -6,13 +6,16 @@ import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 function PhotosVideosTab() {
-	
-
-
+	const [openDialog, setOpenDialog] = React.useState(false);
     const images = [
         "https://www.abantu.constructionmarket-me.com/cmps/public//images/shutterstock_55264171.jpg",
         "https://www.abantu.constructionmarket-me.com/cmps/public//images/Sino-shear-stud-with-chamfer.jpg",
@@ -22,6 +25,14 @@ function PhotosVideosTab() {
         "https://www.abantu.constructionmarket-me.com/cmps/public//images/liebherr-A904C-wheeled-excavator-1.jpg",
         "https://www.abantu.constructionmarket-me.com/cmps/public//images/t3.jpg",
     ]
+
+	const handleClickOpenDialog = () => {
+        setOpenDialog(true);
+      };
+    
+      const handleCloseDialog = () => {
+        setOpenDialog(false);
+      };
 
 	return (
 		<div className="md:flex max-w-2xl">
@@ -34,7 +45,7 @@ function PhotosVideosTab() {
 				
 						<div key={2} className="mb-48">
 							<ListSubheader component="div" className="flex items-center px-0 mb-24">
-								<Typography variant="h6">Pipeline Strainers</Typography>
+								<Typography variant="h6">All MEdia</Typography>
 								
 							</ListSubheader>
 
@@ -51,8 +62,8 @@ function PhotosVideosTab() {
 										<GridListTileBar
 											title="Valves"
 											actionIcon={
-												<IconButton>
-													<Icon className="text-white opacity-75">info</Icon>
+												<IconButton onClick={handleClickOpenDialog}>
+													<Icon className="text-white opacity-75">delete</Icon>
 												</IconButton>
 											}
 										/>
@@ -60,7 +71,29 @@ function PhotosVideosTab() {
 								))}
 							</GridList>
 						</div>
-				
+
+						<Dialog
+							open={openDialog}
+							keepMounted
+							onClose={handleCloseDialog}
+							aria-labelledby="alert-dialog-slide-title"
+							aria-describedby="alert-dialog-slide-description"
+						>
+							<DialogTitle id="alert-dialog-slide-title">{"Deleting Media Files?"}</DialogTitle>
+							<DialogContent>
+							<DialogContentText id="alert-dialog-slide-description">
+								Are you sure you want to delete
+							</DialogContentText>
+							</DialogContent>
+							<DialogActions>
+							<Button onClick={handleCloseDialog} color="primary">
+								No
+							</Button>
+							<Button onClick={handleCloseDialog} color="primary">
+								Yes
+							</Button>
+							</DialogActions>
+						</Dialog>
 				</FuseAnimateGroup>
 			</div>
 		</div>
