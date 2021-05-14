edf23b1bd319a13e2634e88afd9397f8772211ff
Theo Nga <tdbvume@outlook.com>
2021-05-14 11:01:20 +0200

    my posts add button

--- src/app/main/apps/profile/tabs/Posts.js
+++ src/app/main/apps/profile/tabs/Posts.js
@@ -3,14 +3,19 @@ import { makeStyles } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import Post from './Post'
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 export default function Posts(props){
     const classes = useStyles();
 
 
     return(
-       <Grid container className={classes.root} spacing={1}>
-          
+        <div>
+                <Button variant="contained" className={classes.btn} color="primary" href="#outlined-buttons">
+                    Add Product
+                </Button>
+                <Grid container className={classes.root} spacing={1}>
+                
                
                 {Array.from(Array(26).keys()).map((value) => (
                     <Grid key={value} item xs={12} md={3} lg={3}>
@@ -20,6 +25,8 @@ export default function Posts(props){
                
             
        </Grid>
+        </div>
+       
     )
 }
 
@@ -27,21 +34,11 @@ const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
       },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
+      btn:{
+         float: 'right',
+         margin:40,
+         height: 60,
+         width: 200,
+         color: 'black !important'
+      }
   }));
