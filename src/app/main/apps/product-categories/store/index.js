9937df185eb7a6c830fd53635b42051c6a5b1120
Theo Nga <tdbvume@outlook.com>
2021-05-18 18:40:26 +0200

    product category business type slice

--- src/app/main/apps/product-categories/store/index.js
+++ src/app/main/apps/product-categories/store/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit';
 import productCategory from './productCategorySlice';
 import productCategories from './productCategoriesSlice';
 import sectors from './sectorsSlice'
+import BusinessTypes from './businessTypesSlice';
 
 const reducer = combineReducers({
 	productCategories,
 	productCategory,
+	BusinessTypes,
 	sectors
 });
 
